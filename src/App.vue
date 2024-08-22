23897ce76d6251a5d0178fbd943f559ed7fa1e65
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-08-22 22:11:26 +0300

    Small fixes.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <div class="container-fluid">
     <div class="row min-vh-100">
       <div class="col-sm-6 text-center bg-light">
-        <h1 class="mt-5">
-          Login form section
-        </h1>
+        <h1 class="mt-5">Login form section</h1>
       </div>
       <div class="col-sm-6 angled-gradient text-center">
-        <h1 class="mt-5">
-          Image section
-        </h1>
+        <h1 class="mt-5">Image section</h1>
       </div>
     </div>
   </div>
--- src/old/pages/RegisterPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-// import TheWelcome from '../components/HelloWorld.vue';
-</script>
-
-<template>
-  <div class="about">
-    <h1>Register page</h1>
-  </div>
-</template>
