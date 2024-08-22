4bcd91e1dec142b17cf28d4defb382ffd50bdcb4
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-08-22 22:09:31 +0300

    Second preview template.

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import LoginPage from '@/pages/LoginPage.vue';
-import RegisterPage from '@/pages/RegisterPage.vue';
+// import LoginPage from '@/pages/LoginPage.vue';
+// import RegisterPage from '@/pages/RegisterPage.vue';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginPage
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterPage
-    }
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: LoginPage
+    // },
+    // {
+    //   path: '/register',
+    //   name: 'register',
+    //   component: RegisterPage
+    // }
   ]
 });
 
