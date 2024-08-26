d8e71a6fbbb6658ccc0f858f1842a8c54b443a24
Roman Kondrashov <nr.kondrashov@gmail.com>
2024-08-26 22:02:16 +0300

    Add form, rework styles.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,30 @@
 <template>
   <div class="container-fluid angled-gradient">
     <div class="row min-vh-100">
-      <div class="col-sm-6 p-sm-3 p-2 text-center">
+      <div class="col-sm-6 p-sm-5 p-4">
         <div class="card mw-600">
-          <div class="card-body">
+          <div class="card-body p-sm-5 p-4">
             <h5 class="card-title">Card title</h5>
             <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+              </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+            <!--<a href="#" class="card-link">Card link</a>-->
+            <!--<a href="#" class="card-link">Another link</a>-->
           </div>
         </div>
       </div>
